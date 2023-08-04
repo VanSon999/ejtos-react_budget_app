d76529cdf037f743adfb66cf345391f8f8b908de
Nguyễn Văn Sơn <33483193+VanSon999@users.noreply.github.com>
2023-08-04 21:45:40 +0700

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,34 @@
-
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+const Budget = () => {
+    const { budget, remaining, dispatch, currency} = useContext(AppContext);
+    
+    const MAX_BUDGET = 20000;
+    return (
+        <div className='alert alert-secondary'>
+            <span>Budget: {currency}{
+                <input
+                    required='required'
+                    type='number'
+                    id='budget_input'
+                    value={budget}
+                    style={{ size: 10 }}
+                    onChange={(event) => {
+                        let val = 10*event.target.value - 9*budget;
+                        if(val >= MAX_BUDGET){
+                            alert(`Budget cannot exceed ${currency}${MAX_BUDGET}`);
+                        }else if (val < (budget - remaining)){
+                            alert("You cannot reduce the budget value lower than the spending");
+                        }else{
+                            dispatch({
+                                type: 'SET_BUDGET',
+                                payload: val,
+                            });
+                        }
+                    }}>
+                </input>
+            }</span>
+        </div>
+    );
+};
+export default Budget;
